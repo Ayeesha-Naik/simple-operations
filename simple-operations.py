2df9fdaddd2611bb7d52dd060721cb0f54382afe
Ayeesha <01fe24ica024@kletech.ac.in>
2025-10-31 08:05:15 -0700

    Math Operations

---
+++ simple-operations.py
@@ -0,0 +1,13 @@
+# Program to perform addition and subtraction
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing addition and subtraction
+addition = num1 + num2
+subtraction = num1 - num2
+
+# Displaying the results
+print("The sum of the numbers is:", addition)
+print("The difference of the numbers is:", subtraction)
